1ea5588011eab991f5c40f28bec49fbaf606584d
vladaerastova <vladayerastova@gmail.com>
2018-12-01 21:37:43 +0200

    Code Style improvements

--- galcon_game.py
+++ galcon_game.py
@@ -1,8 +1,4 @@
-import os
-import sys
-from galcon_view import GalconView
 from menu import Menu, StartMPMenu, JoinMPMenu, SettingsMenu, PreGameMenu
-from galcon_network import GalconNetwork
 import pygame as pg
 import pygame.display as disp
 import pygame.event as pgevent
@@ -10,8 +6,7 @@ import pygame.time as pgtime
 import pygame.freetype as pgfont
 
 
-
-class GalconGame():
+class GalconGame:
     def __init__(self, w, h):
         self.w = w
         self.h = h
@@ -31,7 +26,7 @@ class GalconGame():
         # create the menu beforehand
         self.mainMenu = Menu(self.w, self.h)
         self.__createMainMenu__()
-        self.showMainMenu()
+        self.show_main_menu()
 
     def run(self):
 
@@ -40,108 +35,104 @@ class GalconGame():
             # check events
             for event in pgevent.get():
                 if event.type == pg.KEYDOWN:
-                    self.keyPressed(event)
+                    self.key_pressed(event)
                 elif event.type == pg.MOUSEMOTION:
-                    self.mouseMove(event)
+                    self.mouse_move(event)
                 elif event.type == pg.MOUSEBUTTONDOWN:
-                    self.mouseDown(event)
+                    self.mouse_down(event)
                 elif event.type == pg.MOUSEBUTTONUP:
-                    self.mouseUp(event)
+                    self.mouse_up(event)
                 elif event.type == pg.QUIT:
                     self.running = False
-            if not self.running: break
+            if not self.running:
+                break
             # step timer
-            self.timerFired()
+            self.timer_fired()
             self.redraw()
             self.clock.tick(30)
 
     def __createMainMenu__(self):
         self.mainMenu = Menu(self.w, self.h)
-        self.mainMenu.starBG()
-        butW = 300
+        self.mainMenu.star_bg()
+        but_w = 300
 
-        self.mainMenu.addLabel("GALCON", self.w // 2, 100,
-                               font=pgfont.SysFont('Tahoma', 32))
-        self.mainMenu.addButton("START MULTIPLAYER", pg.Rect((self.w - butW) // 2, 250,
-                                                butW, 50),
-                                self.showStartMPMenu)
-        self.mainMenu.addButton("JOIN MULTIPLAYER",
-                                pg.Rect((self.w - butW) // 2, 310, butW, 50),
-                                self.showJoinMPMenu)
-        self.mainMenu.addButton("SETTINGS",
-                                pg.Rect((self.w - butW) // 2, 370, butW, 50),
-                                self.showSettings)
-        self.mainMenu.addButton("QUIT",
-                                pg.Rect((self.w - butW) // 2, 430, butW, 50),
-                                self.quitGame)
+        self.mainMenu.add_label("GALCON", self.w // 2, 100, font=pgfont.SysFont('Tahoma', 32))
+        self.mainMenu.add_button("START MULTIPLAYER", pg.Rect((self.w - but_w) // 2, 250, but_w, 50), 
+                                 self.show_start_mp_menu)
+        self.mainMenu.add_button("JOIN MULTIPLAYER", pg.Rect((self.w - but_w) // 2, 310, but_w, 50), 
+                                 self.show_join_mp_menu)
+        self.mainMenu.add_button("SETTINGS", pg.Rect((self.w - but_w) // 2, 370, but_w, 50), 
+                                 self.show_settings)
+        self.mainMenu.add_button("QUIT", pg.Rect((self.w - but_w) // 2, 430, but_w, 50), self.quit_game)
 
-    def showMainMenu(self):
+    def show_main_menu(self):
         self.mainMenu.pressed = None
         self.mode = self.mainMenu
         self.screen.blit(self.mode.bg, (0, 0))
         disp.update()
 
         if not pg.mixer.music.get_busy():
-            self.playMusic()
+            self.play_music()
 
-    def quitGame(self):
+    def quit_game(self):
         self.running = False
 
-    def showSettings(self):
-        self.settingsMenu = SettingsMenu(self.w, self.h, self.user, self.showMainMenu)
+    def show_settings(self):
+        self.settingsMenu = SettingsMenu(self.w, self.h, self.user, self.show_main_menu)
         self.settingsMenu.pressed = None
         self.mode = self.settingsMenu
         self.screen.blit(self.mode.bg, (0, 0))
         disp.update()
 
-    def showStartMPMenu(self):
-        self.startMPMenu = StartMPMenu(self.w, self.h, self.startPreGame, self.showMainMenu)
+    def show_start_mp_menu(self):
+        self.startMPMenu = StartMPMenu(self.w, self.h, self.start_pre_game, self.show_main_menu)
         self.startMPMenu.pressed = None
         self.mode = self.startMPMenu
         self.screen.blit(self.mode.bg, (0, 0))
         disp.update()
 
-    def showJoinMPMenu(self):
-        self.joinMPMenu = JoinMPMenu(self.w, self.h,self.showMainMenu)
+    def show_join_mp_menu(self):
+        self.joinMPMenu = JoinMPMenu(self.w, self.h, self.show_main_menu)
         self.joinMPMenu.pressed = None
         self.mode = self.joinMPMenu
         self.screen.blit(self.mode.bg, (0, 0))
         disp.update()
 
-    def startPreGame(self):
+    def start_pre_game(self):
         self.preGameMenu = PreGameMenu(self.w, self.h)
         self.mode = self.preGameMenu
         self.screen.blit(self.mode.bg, (0, 0))
         disp.update()
 
-    def playMusic(self):
+    def play_music(self):
         pg.mixer.music.load('media/bgm.mp3')
         pg.mixer.music.play(-1)
 
-    def timerFired(self):
-        self.mode.timerFired()
+    def timer_fired(self):
+        self.mode.timer_fired()
 
-    def mouseMove(self, event):
-        self.mode.mouseMove(event)
+    def mouse_move(self, event):
+        self.mode.mouse_move(event)
 
-    def mouseDown(self, event):
-        self.mode.mouseDown(event)
+    def mouse_down(self, event):
+        self.mode.mouse_down(event)
 
-    def mouseUp(self, event):
-        self.mode.mouseUp(event)
+    def mouse_up(self, event):
+        self.mode.mouse_up(event)
 
-    def keyPressed(self, event):
-        self.mode.keyPressed(event)
+    def key_pressed(self, event):
+        self.mode.key_pressed(event)
 
     def redraw(self):
         rects = self.mode.redraw(self.screen)
         disp.update(rects)
 
 
-class User():
+class User:
     def __init__(self, name, color):
         self.name = name
         self.color = color
+
 
 def main():
     game = GalconGame(1024, 768)
