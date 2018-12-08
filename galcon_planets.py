a8e71dc3d8d1cea0a2b1b8ab94d059b051abe2bc
Kirill Redin <redinkirill.ua@gmail.com>
2018-12-08 20:27:00 +0200

    User now can return from game to menu

--- galcon_planets.py
+++ galcon_planets.py
@@ -18,15 +18,25 @@ planets = {
 }
 
 class GameView(Menu):
-    def __init__(self, w, h, screen, user):
+    def __init__(self, w, h, screen, user, main_menu):
         self.w = w
         self.h = h
         self.screen = screen
         self.user = user
+        self.main_menu = main_menu
+        self.buttons = sp.RenderUpdates()
+        self.statusBoxDict = dict()
+        self.statusBoxes = sp.RenderUpdates()
         self.bg = pg.Surface((w, h))
         self.star_bg()
         self.planets = []
         self.active_planet = None
+        self.pressed = None
+        self.exit_menu_shown = False
+
+        self.add_status_box("status", "Exit to main menu?", w // 2, h // 2 - 50)
+        self.add_button("EXIT", pg.Rect((w - 300) // 2, (h - 50) // 2, 300, 50), self.main_menu)
+        self.add_button("BACK", pg.Rect((w - 300) // 2, (h - 50) // 2 + 60, 300, 50), self.hide_exit_menu)
     
     def accept_planets(self, planets):
         for planet in planets:
@@ -37,8 +47,18 @@ class GameView(Menu):
             planet.draw(screen)
         pg.display.update()
 
+    def draw_exit_menu(self, screen):
+        if self.exit_menu_shown:
+            self.buttons.draw(screen)
+            self.statusBoxes.draw(screen)
+
+    def hide_exit_menu(self):
+        self.exit_menu_shown = False
+        self.buttons.clear(self.screen, self.bg)
+        self.statusBoxes.clear(self.screen, self.bg)
+
     def draw_info(self, screen):
-        rect = pg.Rect(0,self.h - 100, 300, 100)
+        rect = pg.Rect(0, self.h - 100, 300, 100)
         screen.fill((0, 0, 0), rect)
         pg.draw.rect(screen, pg.Color("red"), rect, 1)
         if self.active_planet is not None:
@@ -53,23 +73,39 @@ class GameView(Menu):
                 self.active_planet = planet
                 break
 
+        for but in self.buttons:
+            if but.contains_pt(event.pos):
+                but.mouse_over()
+            else:
+                but.un_mouse_over()
+
     def timer_fired(self):
         pass
 
     def mouse_down(self, event):
-        pass
+        if self.exit_menu_shown:
+            for but in self.buttons:
+                if but.contains_pt(event.pos):
+                    self.pressed = but
+                    but.press()
 
     def mouse_up(self, event):
-        pass
+        if self.pressed:
+            if self.pressed.contains_pt(event.pos):
+                self.pressed.release()
+                self.pressed = None
+            else:
+                self.pressed.unpress()
+                self.pressed = None
 
     def key_pressed(self, event):
-        pass
+        if event.key == pg.K_ESCAPE:
+            self.exit_menu_shown = True
 
     def redraw(self, screen):
         self.draw(screen, self.user)
         self.draw_info(screen)
-        
-    
+        self.draw_exit_menu(screen)
 
     def generate_mocked_planets(self, player, enemies): 
         planets = []
@@ -82,6 +118,7 @@ class GameView(Menu):
         planets.append(Planet(3, 850, 600, randint(30, 100), enemies[1]))
 
         return planets
+
 
 class Planet(pg.sprite.Sprite):
 
