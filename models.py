a267bc403d239b243d3f41a8788bfe838465074c
BogdanOros <orossbogdan@gmail.com>
2018-12-15 21:31:18 +0200

    Fixed coloring issues for planets

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import pygame.freetype as pgfont
 import pygame.gfxdraw as gfx
+from random import randint
 
 
 class Color:
@@ -15,16 +16,35 @@ class Color:
     LIGHT_SLATE_BLUE = (127, 127, 255)
     DARK_GREEN = (0, 127, 0)  # it is not real dark
 
+    @staticmethod
+    def colors():
+        return [Color.RED, Color.GREEN, Color.BLUE, Color.WHITE, Color.YELLOW, Color.ORANGE, Color.PURPLE]
+    
+    @staticmethod
+    def random(ignore=None):
+        colors = Color.colors()
+        if ignore is not None:
+            for x in ignore:
+                colors.remove(x)
+        return colors[randint(0, len(colors))]
+        
+
 
 class Planet(pg.sprite.Sprite):
     planet_red_img = pg.image.load('materials/planet_red.png')
     planet_green_img = pg.image.load('materials/planet_green.png')
     planet_blue_img = pg.image.load('materials/planet_blue.png')
+    planet_orange_img = pg.image.load('materials/planet_orange.png')
+    planet_yellow_img = pg.image.load('materials/planet_yellow.png')
+    planet_purple_img = pg.image.load('materials/planet_purple.png')
 
     planets = {
         Color.RED: planet_red_img,
         Color.GREEN: planet_green_img,
-        Color.BLUE: planet_blue_img
+        Color.BLUE: planet_blue_img,
+        Color.ORANGE: planet_orange_img,
+        Color.YELLOW: planet_yellow_img,
+        Color.PURPLE: planet_purple_img,
     }
 
     def __init__(self, id, x, y, radius, owner):
